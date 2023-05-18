36191fa581792ab40c80d4d5f9dc91a4ac955c8d
yoko-2022 <rilapy_029@yahoo.co.jp>
2023-05-18 11:51:21 +0900

    　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　gifに変更　　　　　

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-/* hamクリック処理 */
-$('#ham-btn').on('click', function() {
-  $(this).toggleClass('is-active');
-  let index = $(this).children('span').text();
-  $(this).children('span').text(index === 'menu' ? 'close' : 'menu');
-
-  $('#g-nav').toggleClass('show');
-});
-
-$(window).scroll(function() {
-  let y = $(this).scrollTop();
+$(function() {
+  $('#ham-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+    let index = $(this).children('span').text();
+    $(this).children('span').text(index === 'menu' ? 'close' : 'menu');
   
-  $('#top-btn>a').on('click', function() {
-    $('html,body').animate({scrollTop: 0}, 600);
+    $('#g-nav').toggleClass('show');
+  });
+  
+  $(window).scroll(function() {
+    let y = $(this).scrollTop();
+    
+    $('#top-btn>a').on('click', function() {
+      $('html,body').animate({scrollTop: 0}, 600);
+      return false;
+    })
+    let point1 = $('#skill').offset().top;
+    let point2 = $('#timeline').offset().top;
+  
+    if(y > point1 - 200) {
+      $('#skill>.skill-inner').addClass('show');
+    } else {
+      $('#skill>.skill-inner').removeClass('show');
+    }
+  
+    if(y > point2 - 100) {
+      $('#timeline .timeline-item').addClass('show');
+    } else {
+      $('#timeline .timeline-item').removeClass('show');
+    }
+  
+    if(y > 300) {
+      $('#to-top').addClass('show');
+    } else {
+      $('#to-top').removeClass('show');
+    }
+  });
+
+  $('#to-top>a').on('click', function () {
+    $('html,body').animate({scrollTop: 0}, 200);
     return false;
-  })
-  let point1 = $('#skill').offset().top;
-  let point2 = $('#timeline').offset().top;
-
-  if(y > point1 - 200) {
-    $('#skill>.skill-inner').addClass('show');
-  } else {
-    $('#skill>.skill-inner').removeClass('show');
-  }
-
-  if(y > point2 - 100) {
-    $('#timeline .timeline-item').addClass('show');
-  } else {
-    $('#timeline .timeline-item').removeClass('show');
-  }
-
-  if(y > 300) {
-    $('#to-top').addClass('show');
-  } else {
-    $('#to-top').removeClass('show');
-  }
+  });
 });
-
 
 MicroModal.init({
   disableScroll: true,
